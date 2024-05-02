ca1c2e46930ffa176fdcfcaadeed7b399eef7da2
muminvarici <mumin.varici@outlook.com>
2024-05-02 11:28:05 +0300

    Console app created

---
+++ SimpleApp.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+try
+{
+    Log.Information("Starting application");
+    CreateHostBuilder(args).Build().Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .UseSerilog() // Use Serilog for logging
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            // Configuration sources
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            config.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+            // config.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            config.AddEnvironmentVariables();
+        })
+        .ConfigureServices((hostContext, services) =>
+        {
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(hostContext.Configuration)
+                .CreateLogger();
+
+            Log.Information("Configured Services");
+
+            // Dependency injection
+            services.AddHostedService<Worker>();
+            services.AddTransient<Worker>();
+        });
---
+++ SimpleApp.Console/Worker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Hosted service starting");
+        return Task.CompletedTask;
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Hosted service stoping");
+        return base.StopAsync(cancellationToken);
+    }
+}
